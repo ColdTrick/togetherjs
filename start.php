ab87facd29a2b6f9772b5b771fabdb909b29f793
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-11-20 12:09:25 +0100

    Embeds the together js library on your site and adds a footer-alt link to start collaborating

---
+++ start.php
@@ -0,0 +1,19 @@
+<?php
+
+// register default Elgg events
+elgg_register_event_handler("init", "system", "togetherjs_init");
+elgg_register_event_handler("pagesetup", "system", "togetherjs_pagesetup");
+
+function togetherjs_init(){
+	elgg_register_js("togetherjs", "https://togetherjs.com/togetherjs-min.js");
+}
+
+function togetherjs_pagesetup() {
+	elgg_load_js("togetherjs");
+	elgg_register_menu_item("footer", ElggMenuItem::factory(array(
+				"name" => "togetherjs",
+				"href" => "javascript:TogetherJS(this)",
+				"text" => "Start TogetherJS",
+				"section" => "alt"
+			)));
+}
